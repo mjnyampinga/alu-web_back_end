d3d23cc4143494d87302be2a3ec94be0bd349dc9
nmjoselyne09 <nmjoselyne09@gmail.com>
2024-10-23 18:42:25 +0200

    NoSQL

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""pymongo"""
+"""update school document in a collection"""
 
 
 def update_topics(mongo_collection, name, topics):
-    """changes all topics of a document based on the name"""
-    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    """update school document in a collection"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-""" lists all documents in a collection """
-
+"""get all the data from a collection"""
 
 def list_all(mongo_collection):
-    """lists all documents in a collection"""
-    documents = mongo_collection.find()
-    return documents
+    """list all documents in a collection"""
+    return list(mongo_collection.find())
